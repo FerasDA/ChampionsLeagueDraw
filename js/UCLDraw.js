c36f1d1be0edda5aeb65762b3a9770cd4de18d2e
Feras Deiratany <deiratany.1@osu.edu>
2015-11-08 00:54:54 -0500

    main in js and added to html

--- js/UCLDraw.js
+++ js/UCLDraw.js
@@ -1,3 +1,20 @@
+var Ucl = Ucl || {};
+
+$(document).ready(function() {
+
+  draw();
+
+  // main();
+
+});
+
+
 function draw() {
   //populate a table of a random draw
+
+  $('#draw').on('click', function() {
+    //var team = Ucl.teams.data[0];
+
+  });
+
 }
